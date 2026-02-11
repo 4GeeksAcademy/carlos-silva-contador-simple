6ef392b95f959d7b8a3e9595a13a719a684e974d
Carlos Silva <carlhmath@gmail.com>
2026-02-11 23:07:53 +0000

    ajustes!

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,25 +1,27 @@
 const SimpleCounter = (porps) => {
     const { numberOne, numberTwo, numberTree, numberFor, numberFive, numberSix } = porps
     return (
-      <div className="container text-center col-6 bg-black p-4  text-black fs-2 border border-4">
-            <div className="row ">
-                <div className="col-2 bg-secondary border border-2 p-2">
-                  <i class="fa-solid fa-clock"></i>
-                </div>
-                <div className="col-2 bg-secondary  border border-2 p-2">
-                    <div>{Math.floor(numberFive % 10)}</div>
-                </div>
-                <div className="col-2 bg-secondary  border border-2 p-2 ">
-                    <div>{Math.floor(numberFor % 10)}</div>
-                </div>
-                <div className="col-2 bg-secondary   border border-2 p-2">
-                    <div>{Math.floor(numberTree % 10)}</div>
-                </div>
-                <div className="col-2 bg-secondary  border border-2 p-2 ">
-                    <div>{Math.floor(numberTwo % 10)}</div>
-                </div>
-                <div className="col-2 bg-secondary  border border-2  p-2">
-                    <div>{Math.floor(numberOne % 10)}</div>
+        <div className="container text-center col-6 bg-black p-4  text-black fs-2 border border-4">
+            <div className="row">
+                <div className=" col-12 number-clock ">
+                    <div className=" bg-secondary border border-2 ">
+                        <i class="fa-solid fa-clock"></i>
+                    </div>
+                    <div className=" bg-secondary  border border-2 ">
+                        {Math.floor(numberFive % 10)}
+                    </div>
+                    <div className=" bg-secondary  border border-2  ">
+                        {Math.floor(numberFor % 10)}
+                    </div>
+                    <div className=" bg-secondary   border border-2 ">
+                        {Math.floor(numberTree % 10)}
+                    </div>
+                    <div className=" bg-secondary  border border-2  ">
+                        {Math.floor(numberTwo % 10)}
+                    </div>
+                    <div className=" bg-secondary  border border-2  ">
+                        {Math.floor(numberOne % 10)}
+                    </div>
                 </div>
             </div>
         </div>
